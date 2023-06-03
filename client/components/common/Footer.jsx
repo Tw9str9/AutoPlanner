34d166f23659d15dcc8c9352aeafe4da93297a5d
Tw9str <i88b@hotmail.com>
2023-06-03 16:43:54 +0300

    footer

--- client/components/common/Footer.jsx
+++ client/components/common/Footer.jsx
@@ -1,5 +1,6 @@
 import Image from "next/image";
 import ContactInfo from "../widgets/ContactInfo";
+import Link from "next/link";
 
 export default function Footer() {
   return (
@@ -32,6 +33,43 @@ export default function Footer() {
               Auto<span>Planner</span> NL
             </h1>
             <ContactInfo whatsapp email />
+          </div>
+          <div className="links col">
+            <h2>Links</h2>
+            <ul>
+              <li>
+                <Link href="/">Home</Link>
+              </li>
+              <li>
+                <Link href="/occasions">Occasions</Link>
+              </li>
+              <li>
+                <Link href="/diensten">Diensten</Link>
+              </li>
+              <li>
+                <Link href="/over-ons">Over ons</Link>
+              </li>
+              <li>
+                <Link href="/contact">Contact</Link>
+              </li>
+            </ul>
+          </div>
+          <div className="services col">
+            <h2>Diensten</h2>
+            <ul>
+              <li>
+                <Link href="/diensten#voordelen">Voordelen</Link>
+              </li>
+              <li>
+                <Link href="/diensten#inkopen">Inkopen</Link>
+              </li>
+              <li>
+                <Link href="/diensten#garantie">Garantie</Link>
+              </li>
+              <li>
+                <Link href="/diensten#importeren">Auto Importeren</Link>
+              </li>
+            </ul>
           </div>
         </div>
       </div>
